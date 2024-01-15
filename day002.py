bbd07ccc9cafbee737e4f0835c5ab5457822d985
minson0514 <minson0514@gmail.com>
2024-01-15 11:31:19 +0900

    0.5

--- day002.py
+++ day002.py
@@ -1,17 +1,9 @@
-# SyntaxError: cannot assign to literal here.
-#"univ" = "Inha University"
-# 99 = 71
+x=2
+y=x+5
+print(y)
 
-# Ok
-# case-sensitive
-abc = 7
-Abc = 8
-ABC = 6
-print(abc, ABC, Abc)
-
-test9 = 77
-# 9test = 77
-_9test = 71
-print(test9, _9test)
-
-#False = 123  # reserved words
+print(type(3.14))
+print(type(3.14)==float)
+print(isinstance(3.14,float))
+print(isinstance("Inha", float))
+print(isinstance(55,float))
