7a7ea4d787b757965fdff0a43061cecc2f9849be
minson0514 <minson0514@gmail.com>
2024-01-16 11:34:09 +0900

    w1.0 day03

--- day003.py
+++ day003.py
@@ -15,7 +15,53 @@ print((int(numbers[0]) + int(numbers[1]))) #두 숫자 간의 결합
 '''
 
 #Combine by using join()
+'''
 subjects = ["python", "c++", "database"]
 subjects_string = " / ".join(subjects)
 print(subjects_string)#python / c++ / database
 #list 안의 elements 사이에 " / "가 추가되어서 리턴됨
+'''
+
+#substitute by using replace
+#아래 방법은 원본이 바뀌지는 않음
+'''
+course = "2024 KEB bootcamp"
+print((course.replace('KEB','inha')))#2024 inha bootcamp
+print((course))
+'''
+#원본을 바꾸는 방법
+'''
+course = "2024 KEB bootcamp"
+print((course))
+course=course.replace('KEB', "Inha")
+print((course))
+'''
+
+#여러개 중 하나만 바꾸고 싶을 때
+'''
+course = "KEB 2024 KEB bootcamp KEB"
+print(course)
+course=course.replace('KEB', "Inha", 1)#3번째 자리는 count 몇 개 바꾸는 지
+print(course)
+'''
+
+#strip
+'''
+course = "KEB #2024 KEB !bootcamp KEB...*!#"
+print(course)
+course=course.replace('KEB', "Inha", 2)#3번째 자리는 count 몇 개 바꾸는 지
+print(course)
+print(course.strip("!#.*")) #연속적으로 있는 애들만 지워줌
+'''
+
+#find & index
+'''
+course = "* KEB 2024# KEB !bootcamp KEB...*!#"
+print(course.find('KEB')) #2
+print(course.rfind('KEB')) #26
+print(course.index('KEB')) #2
+print(course.rindex('KEB')) #26
+print(course.find('Inha')) #find는 없으면 -1 반환
+print(course.index('Inha')) #error -> index는 없으면 에러 발생
+'''
+
