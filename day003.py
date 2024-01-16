d6d13dd0306382e9c68602299dfff9a00e271b35
minson0514 <minson0514@gmail.com>
2024-01-16 13:47:53 +0900

    0116_v0.1

--- day003.py
+++ day003.py
@@ -1,6 +1,7 @@
-#예외처리 preview
+#앞에 is 가 붙은 함수는 true false를 리턴함
 '''
 subjects = "python c++ database linux"
+print(subjects.isalnum()) #자주쓰는 함수는 아님
 subject = input("수강신청과목 입력 : ")
 
 try:
@@ -9,3 +10,20 @@ try:
 except ValueError:
     print(f'해당 과목이 존재하지 않습니다.')
 '''
+
+#print('%e' %0.7045)
+
+#default 매개변수
+#format함수 안에 디폴트 매개변수를 줌
+#cf 'the {thing} is in the {place}'.format(thing='duck', place = 'bathtub')
+
+#{"0[]"}.format()
+'''
+subjects = {'python' : "kim", "c++" : 'sung', 'data structure' : "kim", "database" : "kang"}
+print("{0[python]} {0[data structure]}".format(subjects))
+#정렬 시 ^ 이건 중앙 정렬이라는 뜻
+'''
+
+#f string이 최신 스타일 가장 편함
+#capitalize-> 맨 앞글자만 대문자로 바꿈
+#upper-> 전체 다 대문자로 바꿈
